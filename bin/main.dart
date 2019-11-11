893e0cd7fd83d1446d28f76a85a1fbc6ad26b58a
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-11-11 13:30:16 +0530

    Ashish's  Code refactored

--- bin/main.dart
+++ bin/main.dart
@@ -20,101 +20,62 @@
 //  [ 6, 2, 5, 9, 4, 8, 1, 3, 7 ],
 //  [ 8, 7, 3, 5, 1, 2, 9, 6, 4 ]
 //  ]) ➞ true
-bool emptyBoard(List<List<int>>board){
-  return(board.isNotEmpty);
+transposeMatrix(List<List> matrix) {
+  List<List> transpose = List<List>();
+  for (int i = 0; i < matrix[0].length; i++) {
+    List newList = List();
+    matrix.forEach((list) => {newList.add(list[i])});
+    transpose.add(newList);
+  }
+  return transpose;
 }
 
-bool checkBoardLengthIs9(List<List<int>>board){
-  if(!(board.length==9)) //check if 9 rows are present
-      {
-        return false;
-      }
-  else {
-    for (int i = 0; i < 9; i++) {
-      if (!(board[i].length == 9)) //check if 9 columns are present
-          {
-        return false;
-      }
+checkList(List<List<int>> board, int i) {
+
+  return !(board[i].toSet().length!=9);
+}
+
+bool notInRow(List<List<int>> board, int row) {
+  return checkList(board, row);
+}
+
+bool notInCol(List<List<int>> board, int col) {
+  List transposed = transposeMatrix(board);
+  return checkList(board, col);
+}
+
+bool notInBox(List<List<int>> board, int row, int col) {
+
+  List<int> st = [];
+  for(int i=0;i<9;i=i+3){
+    for(int j=0;j<9;j=j+3){
+        int curr=board[i][j];
+        if (st.contains(curr)) {
+          return false;
+        } else {
+          st.add(curr);
+          print(st);
+        }
     }
   }
-  
-    return true;
-  
 }
-bool checkBoardRange(List<List<int>>board){
-  for(int i=0;i<9;i++){
-    for(int j=0;j<9;j++){
-      if(board[i][j]<1 || board[i][j]>9){
-        return false;
-      }
-    }
-    return true;
-  }
+
+bool isValid(List<List<int>> board, int row, int col) {
+  return notInRow(board, row) &&
+      notInCol(board, col);
 }
-bool checkForRows(List<List<int>>board){
-  for(int i=0;i<9;i++){
-    for(int j=1;j<10;j++){
-      if(!board[i].contains(j)){
-        print('row error');
+
+bool sudokuValidator(List<List<int>> board) {
+  int n = 9;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (!isValid(board, i, j) && notInBox(board, i, j)) {
         return false;
       }
     }
   }
   return true;
 }
-bool checkForColumns(List<List<int>>board){
-  List<int> tempList=[];
-  for(int j=0;j<9;j++){
-    for(int i=0;i<9;i++){
-      tempList.add(board[i][j]);
-    }//tempList created
-    for(int index=1;index<10;index++){
-      if(!tempList.contains(index)){
-        print("column error");
-        return false;
-      }
-    }
-  }
-  return true;
-}
-bool subBoardCheck(int row, int col,List<List<int>>board, int boardIndex){
-  List<int> tempList=[];
-  for(int i=row;i<row+3;i++){
-    for(int j=col;j<col+3;j++){
-      tempList.add(board[i][j]);
-    }
-  }
-  for (int index = 1; index < 10; index++)  {
-    if (!tempList.contains(index)) {
-      print(" $boardIndex board error");
-      return false;
-    }
-  }
-  return true;
-}
-
-bool everySubBoardCheck(List<List<int>>board){
-  int boardIndex=1;
-  bool flag=true;
- for(int i=0;i<9;i=i+3){
-   for(int j=0;j<9;j=j+3){
-     flag=flag&&subBoardCheck(i, j, board, boardIndex);
-     boardIndex++;
-   }
- }
- return flag;
-}
-bool sudokuValidator(List<List<int>>board){
-
-  return(  everySubBoardCheck(board)&&
-      emptyBoard(board)       &&
-      checkBoardLengthIs9(board) &&
-      checkBoardRange(board)  &&
-      checkForColumns(board)  &&
-      checkForRows(board)) ;
-
-}
-
 
 // Challenge 3
 // Sort by Factor Length
@@ -125,58 +86,51 @@ bool sudokuValidator(List<List<int>>board){
 // In the example below, since 13 and 7 both have only 2 factors, we put 13 ahead of 7.
 //
 // factorSort([9, 7, 13, 12]) ➞ [12, 9, 13, 7]
-List<int> factorSort(List<int>inputList){
-  //check for negative numbers
-  for(int i=0;i<inputList.length;i++){
-    if(inputList[i]<0){
-      return [];
-    }
-  }
-  for(int i=0;i<inputList.length-1;i++){
-    for(int j=i+1;j<inputList.length;j++){
-      if(factorLength(inputList[i])<factorLength(inputList[j])){
-        //swap
-        int temp=inputList[i];
-        inputList[i]=inputList[j];
-        inputList[j]=temp;
-      }
-      else if(factorLength(inputList[i])==factorLength(inputList[j]))
-      {
-        if(inputList[i]<inputList[j]){
-          int temp=inputList[i];
-          inputList[i]=inputList[j];
-          inputList[j]=temp;
-        }
-      }
-    }
-  }
-  return inputList;
+
+// Sort an array of numbers according
+// to number of factors.
+
+List factorSort(List<int> numbers) {
+  numbers.sort(comparator);
+  return numbers;
 }
-int factorLength(int number){
-  List<int> factorList=[];
-  for(int i=1;i<=number;i++){
-    if(number%i==0){
-      factorList.add(i);
+
+int comparator(int a, int b) {
+  int aFactorCount = 0;
+  int bFactorCount = 0;
+  for (int i = 1; i < a; i++) {
+    if (a % i == 0) {
+      aFactorCount++;
     }
   }
-  // print("Printin factors of $number");
-  // print(factorList);
-  return factorList.length;
+  for (int i = 1; i < b; i++) {
+    if (b % i == 0) {
+      bFactorCount++;
+    }
+  }
+  if (aFactorCount < bFactorCount) return 1;
+  if (aFactorCount > bFactorCount) return -1;
+  if (aFactorCount == bFactorCount) {
+    if (a < b) return 1;
+    if (a == b) return 0;
+    if (a > b) return -1;
+  }
 }
 
 
 main() {
-  print(sudokuValidator(
-      [
-        [ 1, 5, 2, 4, 8, 9, 3, 7, 6 ],
-        [ 7, 3, 9, 2, 5, 6, 8, 4, 1 ],
-        [ 4, 6, 8, 3, 7, 1, 2, 9, 5 ],
-        [ 3, 8, 7, 1, 2, 4, 6, 5, 9 ],
-        [ 5, 9, 1, 7, 6, 3, 4, 2, 8 ],
-        [ 2, 4, 6, 8, 9, 5, 7, 1, 3 ],
-        [ 9, 1, 4, 6, 3, 7, 5, 8, 2 ],
-        [ 6, 2, 5, 9, 4, 8, 1, 3, 7 ],
-        [ 8, 7, 3, 5, 1, 2, 9, 6, 4 ]
-      ]));
+
+  print (sudokuValidator([
+    [ 1, 5, 2, 4, 8, 9, 3, 7, 6 ],
+    [ 7, 3, 9, 2, 5, 6, 8, 4, 1 ],
+    [ 4, 6, 8, 3, 7, 1, 2, 9, 5 ],
+    [ 3, 8, 7, 1, 2, 4, 6, 5, 9 ],
+    [ 5, 9, 1, 7, 6, 3, 4, 2, 8 ],
+    [ 2, 4, 6, 8, 9, 5, 7, 1, 3 ],
+    [ 9, 1, 4, 6, 3, 7, 5, 8, 2 ],
+    [ 6, 2, 5, 9, 4, 8, 1, 3, 7 ],
+    [ 8, 7, 3, 5, 1, 2, 9, 6, 4 ]
+  ]));
   print(factorSort([9, 7, 13, 12]));
+
 }
